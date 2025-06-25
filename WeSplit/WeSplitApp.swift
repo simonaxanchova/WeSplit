e4e10c8616982da47595bbbd42621ca21d44bd15
Simona Anchova <simonaanchova@Simonas-MacBook-Air.local>
2025-06-25 21:00:35 +0200

    Add project files

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Simona Anchova on 17.6.25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
